59c65d8f6298a4ddc275165b09509b2e274a8864
guy-sha <77299650+guy-sha@users.noreply.github.com>
2022-06-05 18:08:46 +0300

    parse additional args

--- server.c
+++ server.c
@@ -11,23 +11,55 @@
 // Most of the work is done within routines written in request.c
 //
 
+RQPolicy strToPolicy(char* str) {
+    if (strcmp(str, "block") == 0)
+        return RQ_BLOCK;
+    if (strcmp(str, "dt") == 0)
+        return RQ_DROP_TAIL;
+    if (strcmp(str, "dh") == 0)
+        return RQ_DROP_HEAD;
+    if (strcmp(str, "random") == 0)
+        return RQ_DROP_RANDOM;
+
+    return RQ_BLOCK;
+}
+
 // HW3: Parse the new arguments too
-void getargs(int *port, int argc, char *argv[])
+void getargs(int* port, int* threads, int* max_queue_size, RQPolicy* policy, int argc, char* argv[])
 {
-    if (argc < 2) {
-	fprintf(stderr, "Usage: %s <port>\n", argv[0]);
-	exit(1);
+    if (argc < 5) {
+        fprintf(stderr, "Usage: %s <port>\n", argv[0]);
+        exit(1);
     }
+    // consider enforcing port number to be > 1023
     *port = atoi(argv[1]);
+    long temp = strtol(argv[2], NULL, 10);
+    if (temp <= 0) {
+        fprintf(stderr, "Invalid arg: threads == %s\n", argv[2]);
+        exit(1);
+    }
+    *threads = (int)temp;
+
+    temp = strtol(argv[3], NULL, 10);
+    if (temp <= 0) {
+        fprintf(stderr, "Invalid arg: max_queue_size == %s\n", argv[3]);
+        exit(1);
+    }
+    *max_queue_size = (int)temp;
+    *policy = strToPolicy(argv[4]);
+
 }
 
 
 int main(int argc, char *argv[])
 {
-    int listenfd, connfd, port, clientlen;
+    int port, threads, max_queue_size;
+    RQPolicy policy;
+    int listenfd, connfd, clientlen;
     struct sockaddr_in clientaddr;
 
-    getargs(&port, argc, argv);
+    getargs(&port, &threads, &max_queue_size, &policy, argc, argv);
+    requestQueue* queue = RQInit(max_queue_size, policy);
 
     // 
     // HW3: Create some threads...

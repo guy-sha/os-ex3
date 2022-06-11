19712b05747e45056dcde8430ac41a55d522116b
Tal-Shab <tal.shab@campus.technion.ac.il>
2022-06-11 19:07:43 +0300

    update requestHandle signature and includes

--- request.h
+++ request.h
@@ -1,5 +1,8 @@
 #ifndef __REQUEST_H__
+#define __REQUEST_H__
+#include "threadStats.h"
+#include "requestQueue.h"
 
-void requestHandle(int fd);
+void requestHandle(req_info req, thread_stats* stats);
 
 #endif
